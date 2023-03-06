d229b51a83786cfb75625986e31529c95c640071
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2023-03-06 19:48:32 +0100

    UPdated

---
+++ api/tests/Unit/Services/UserServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    public function test_it_reads_weather_data_from_cache(): void
+    {
+        $this->markTestSkipped();
+    }
+}
